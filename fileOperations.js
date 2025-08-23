417464502f162f27e569f6406f0cc98b2be3fdf7
evolentDelight <evolentDelight@outlook.com>
2025-08-23 12:31:05 -0700

    Add structure according to its purpose

--- fileOperations.js
+++ fileOperations.js
@@ -1,1 +1,9 @@
 import * as fs from "fs";
+
+//Handles JSON data. Returns nonJSON object
+
+function fileExists(pathname) {}
+
+function readFileSync(pathname) {} //Synchronously read file
+
+function writeFile(pathname, data) {} //Asynchronously write file
